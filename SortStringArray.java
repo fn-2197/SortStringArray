d0aad51c1e3eac520e12706399aaf0c81a9eddfa
fn-2197 <LoganHaas@outlook.com>
2016-10-20 12:16:06 -0600

    Add files via upload

---
+++ SortStringArray.java
@@ -0,0 +1,16 @@
+package gamesnstuff;
+
+import java.util.Arrays;
+
+public class SortStringArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String []ss = {"Bill", "Mary", "Lee", "Agnes", "Alfred", "Thomas", "Alvin", "Bernard", "Ezra", "Herman"};
+Arrays.sort(ss);
+for(int i = 0; i < ss.length; i++){
+	System.out.println(ss[i]);
+}
+	}
+
+}
